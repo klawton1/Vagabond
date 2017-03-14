97f513cd2b9880b704de4150324f340dfeb784e1
rccacho <rccacho@gmail.com>
2017-03-14 15:35:36 -0700

    add cities to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cities = Cites.create([
+	{ name: "San Francisco", image: "#"}, 
+	{ name: "London", image: "#"},
+	{ name: "Gibraltar", image:"#"}
+	])
+
