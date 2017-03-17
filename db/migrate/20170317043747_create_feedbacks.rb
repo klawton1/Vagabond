dc9c1bd293154134a954709f057ded2ab1281999
klawton1 <klawton1@gmail.com>
2017-03-17 10:52:28 -0700

    added feedback routes and controller

--- db/migrate/20170317043747_create_feedbacks.rb
+++ db/migrate/20170317043747_create_feedbacks.rb
@@ -1,6 +1,7 @@
 class CreateFeedbacks < ActiveRecord::Migration[5.0]
   def change
     create_table :feedbacks do |t|
+      t.string :text
       t.belongs_to :user, foreign_key: true
       t.belongs_to :comment, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315221025) do
+ActiveRecord::Schema.define(version: 20170317043747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20170315221025) do
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
+  create_table "feedbacks", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.integer  "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_feedbacks_on_comment_id", using: :btree
+    t.index ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "image",           default: "https://avatars3.githubusercontent.com/u/1489337?v=3&s=460"
@@ -45,4 +55,6 @@ ActiveRecord::Schema.define(version: 20170315221025) do
 
   add_foreign_key "comments", "cities"
   add_foreign_key "comments", "users"
+  add_foreign_key "feedbacks", "comments"
+  add_foreign_key "feedbacks", "users"
 end
