06fc15d4a832400e9536e8d8c35044b95633ae3d
klawton1 <klawton1@gmail.com>
2017-03-16 21:57:48 -0700

    created feedback model

---
+++ db/migrate/20170317043747_create_feedbacks.rb
@@ -0,0 +1,10 @@
+class CreateFeedbacks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :feedbacks do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :comment, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
